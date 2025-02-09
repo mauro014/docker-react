0cf8c10f683e3ff36e949ab4132938687405ce59
mauro014 <mauro014@gmail.com>
2025-02-09 20:08:28 +0200

    Testing change in master to merge in azureCiACR.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. This is Mauricio. Second time. First deploy in azure.
+          Edit <code>src/App.js</code> and save to reload. This is Mauricio. Second time. Testing deploy to azure con ACR.
         </p>
         <a
           className="App-link"
