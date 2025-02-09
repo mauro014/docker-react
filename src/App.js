806b5909b6f9273ee0052b069f24b5a1845d685f
mauro014 <mauro014@gmail.com>
2025-02-09 21:01:24 +0200

    content change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. This is Mauricio. Second time. Testing deploy to azure con ACR with continues doployment.
+          Edit <code>src/App.js</code> and save to reload. This is Mauricio.
         </p>
         <a
           className="App-link"
